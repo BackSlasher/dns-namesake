e68e9d68f148ab857e6b6d2f8c4d95e18a2eb960
nitz <nitz.raz@gmail.com>
2016-09-24 12:59:37 +0300

    Completed args, seem to be working
    Now I need the file arg and practically done

--- dns_namesake/__init__.py
+++ dns_namesake/__init__.py
@@ -21,9 +21,20 @@ def main():
     parser.add_argument('--a', action='append', type=generic_record, help='A records', default=[])
     parser.add_argument('--aaaa', action='append', type=generic_record, help='AAAA records', default=[])
     parser.add_argument('--cname', action='append', type=generic_record, help='CNAME records', default=[])
+    parser.add_argument('-p', '--port', type=int, help='Port to listen on', default=10053)
+    parser.add_argument('--tcp', action='store_true', help='listen in TCP as well as UDP')
+    parser.add_argument('--resolv', help='Resolv conf path for unknown queries. Set to empty to avoid using it', default='/etc/resolv.conf')
+    parser.add_argument('-f', '--forward', type=str, action='append', help='Forward unknown queries to this server')
     args = parser.parse_args()
 
-    # Add to record store
+    # Extract server config from args
+    port = args.port
+    listen_tcp = args.tcp
+    resolv = args.resolv
+    if not resolv: resolv=None
+    forward_servers=args.forward
+
+    # Extract records from args
     rs = RecordStore()
     for r in args.a:
         rs.add(r[0], dns.A, r[1])
@@ -37,15 +48,16 @@ def main():
             FollowResolver( child_resolvers=[
                 StoreResolver(rs),
                 # TODO handle other cases where we need different upstreams
-                client.Resolver(resolv='/etc/resolv.conf'),
+                client.Resolver(resolv=resolv, servers=forward_servers),
             ]),
         ],
     )
 
     protocol = dns.DNSDatagramProtocol(controller=factory)
 
-    reactor.listenUDP(10053, protocol)
-    reactor.listenTCP(10053, factory)
+    reactor.listenUDP(port, protocol)
+    if listen_tcp:
+        reactor.listenTCP(port, factory)
 
     reactor.run()
 
