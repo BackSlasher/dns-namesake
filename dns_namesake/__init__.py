efc600ccce3aebe47265755bb322a0d78c86b63e
nitz <nitz.raz@gmail.com>
2016-09-24 12:45:01 +0300

    Correctly parsing arguments
    Removed testing records

--- dns_namesake/__init__.py
+++ dns_namesake/__init__.py
@@ -1,3 +1,5 @@
+import argparse
+
 from twisted.internet import reactor
 from twisted.names import client, dns, server
 
@@ -5,18 +7,36 @@ from store_resolver import StoreResolver
 from record_store import RecordStore
 from follow_resolver import FollowResolver
 
+SEPARATOR=','
+
+# Arg helpers
+def generic_record(inp):
+    name, payload = inp.split(SEPARATOR)
+    return (name, payload)
+
 def main():
 
-    rs = RecordStore()
+    # Args
+    parser = argparse.ArgumentParser(description='DNS filter')
+    parser.add_argument('--a', action='append', type=generic_record, help='A records', default=[])
+    parser.add_argument('--aaaa', action='append', type=generic_record, help='AAAA records', default=[])
+    parser.add_argument('--cname', action='append', type=generic_record, help='CNAME records', default=[])
+    args = parser.parse_args()
 
-    rs.add('bla.google.com', dns.CNAME, 'google.com')
-    rs.add('blu.google.com', dns.A, '1.2.3.4')
-    rs.add(r'/(.+)\.cnet.com/', dns.CNAME, r'\1.google.com')
+    # Add to record store
+    rs = RecordStore()
+    for r in args.a:
+        rs.add(r[0], dns.A, r[1])
+    for r in args.aaaa:
+        rs.add(r[0], dns.AAAA, r[1])
+    for r in args.cname:
+        rs.add(r[0], dns.CNAME, r[1])
 
     factory = server.DNSServerFactory(
         clients=[
             FollowResolver( child_resolvers=[
                 StoreResolver(rs),
+                # TODO handle other cases where we need different upstreams
                 client.Resolver(resolv='/etc/resolv.conf'),
             ]),
         ],
